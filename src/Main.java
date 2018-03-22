885fc54202a80c16695411fd97a1afb059eb9fb7
Kazi Emrul Kayes Emon <kek.emon@samsung.com>
2018-03-22 10:20:21 +0600

    Text Editor

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("This is a message");
+		TextEditor textEditor = new TextEditor();
+		String str = "Emon";
+		textEditor.insert(0, str.toCharArray(), str.length());
+//		str = "Palsh";
+//		textEditor.insert(2, str.toCharArray(), str.length());
+		textEditor.delete(1, 1);
+		
+		char[] text = textEditor.read(0, 10);
+		System.out.println(text);
+		
 	}
 }
