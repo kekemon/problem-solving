6677755e6d9b5a89e43fd0395fae17d968c3d506
Kazi Emrul Kayes Emon <kek.emon@samsung.com>
2018-04-23 15:34:23 +0600

    New Code

---
+++ src/AddAll.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+
+public class AddAll {
+	static class MinHeap{
+		private long minHeap[];
+		private int size = 0;
+		
+		public MinHeap(int maxSize) {
+			minHeap = new long[maxSize + 1];
+		}
+		
+		public void insert(long value){
+			minHeap[++size] = value;
+			bubbleUp(size);
+		}
+		
+		public long extractMin(){
+			long min = minHeap[1];
+			delete(1);
+			return min;
+		}
+		
+		public boolean isEmpty(){
+			return size == 0;
+		}
+		
+		private void delete(int position){
+			if(position>0){
+				minHeap[position] = minHeap[size--];
+				sinkDown(position);
+			}
+		}
+		
+		private void bubbleUp(int position){
+			int parentPos = position/2;
+			if(parentPos>0 && minHeap[parentPos]>minHeap[position]){
+				swap(parentPos, position);
+				bubbleUp(parentPos);
+			}
+		}
+		
+		private void swap(int pos1, int pos2) {
+			long tmp = minHeap[pos1];
+			minHeap[pos1] = minHeap[pos2];
+			minHeap[pos2] = tmp;
+		}
+
+		private void sinkDown(int position){
+			int minPos = position;
+			int leftPos = position*2;
+			int rightPos = leftPos + 1;
+			
+			if(leftPos <= size && minHeap[leftPos]<minHeap[minPos]){
+				minPos = leftPos;
+			}
+			
+			if(rightPos <= size && minHeap[rightPos]<minHeap[minPos]){
+				minPos = rightPos;
+			}
+			
+			if(minPos != position){
+				swap(minPos, position);
+				sinkDown(minPos);
+			}
+		}
+	}
+	
+	private static long getAddCost(MinHeap minHeap) {
+		long minCost = 0;
+		while(!minHeap.isEmpty()){
+			long min1 = minHeap.extractMin();
+			long min2 = 0;
+			if(!minHeap.isEmpty()){
+				min2 = minHeap.extractMin();
+			}
+			long cost = min1 + min2; 
+			minCost+= cost;
+			if(!minHeap.isEmpty()){
+				minHeap.insert(cost);
+			}
+		}
+		return minCost;
+	}
+	
+	public static void main(String[] args) {
+		Scanner in = InputUtil.getScanner();
+//		Scanner in = new Scanner(System.in);
+		
+		int N;
+		while((N = in.nextInt()) != 0){
+			MinHeap minHeap = new MinHeap(N);
+			for (int i = 0; i < N; i++) {
+				minHeap.insert(in.nextInt());
+			}
+			System.out.println(getAddCost(minHeap));
+		}
+	}
+	
+}
---
+++ src/ForestTrees.java
@@ -0,0 +1,135 @@
+import java.util.Scanner;
+
+public class ForestTrees {
+	
+	private static Node hashMap[];
+	
+	static class Node{
+		
+		private Node parent;
+		private char value;
+		private int count;
+		private boolean isParent;
+		
+		public Node(char value) {
+			this.setValue(value);
+			this.setParent(this);
+			this.setCount(1);
+			this.setParent(true);
+		}
+
+		public Node getParent() {
+			if(!this.equals(parent))
+				return parent.getParent();
+			return parent;
+		}
+
+		public void setParent(Node parent) {
+			this.parent = parent;
+		}
+
+		public char getValue() {
+			return value;
+		}
+
+		public void setValue(char value) {
+			this.value = value;
+		}
+
+		public int getCount() {
+			return count;
+		}
+
+		public void setCount(int count) {
+			this.count = count;
+		}
+
+		public boolean isParent() {
+			return isParent;
+		}
+
+		public void setParent(boolean isParent) {
+			this.isParent = isParent;
+		}
+	}
+	
+	private static int getValueByKey(char key){
+		return key - 'A';
+	}
+	
+	private static Node getNode(char ch){
+		int index = getValueByKey(ch);
+		if(hashMap[index] == null){
+			hashMap[index] = new Node(ch);
+			return hashMap[index];
+		}
+		return hashMap[index];
+	}
+	
+	public static void makeSet(char ch1, char ch2){
+		Node paNode1 = getNode(ch1).getParent();
+		Node paNode2 = getNode(ch2).getParent();
+		
+		if(paNode1.equals(paNode2)) return;
+		
+		if(paNode1.getCount() > paNode2.getCount()){
+			paNode2.setParent(false);
+			paNode2.setParent(paNode1);
+			paNode1.setCount(paNode1.getCount() + paNode2.getCount());
+		}else{
+			paNode1.setParent(false);
+			paNode1.setParent(paNode2);
+			paNode2.setCount(paNode1.getCount() + paNode2.getCount());
+		}
+	}
+	
+	private static boolean isLetter(char c){
+		if(c >='A' && c <='Z') return true;
+		return false;
+	}
+	
+	private static void parseInput(char[] line) {
+		char[] pair = new char[2];
+		for (int j = 0,k = 0; j < line.length && k<=1; j++) {
+			if(isLetter(line[j])){
+				pair[k++] = line[j];
+			}
+		}
+		makeSet(pair[0], pair[1]);
+		
+		
+	}
+	
+	public static void main(String[] args) {
+//		Scanner in = InputUtil.getScanner();
+		Scanner in = new Scanner(System.in);
+		int TC = in.nextInt();
+		for (int i = 0; i < TC; i++) {
+			hashMap = new Node[26];
+			
+			while(in.hasNext()){
+				char[] line = in.next().toCharArray();
+				if(line[0]== '*') break;
+				parseInput(line);
+			}
+			char[] data = in.next().toCharArray();
+			int tree = 0;
+			int acorn = 0;
+			for (int j = 0; j < data.length; j++) {
+				if(isLetter(data[j])){
+					int index = getValueByKey(data[j]);
+					if(hashMap[index] != null){
+						if(hashMap[index].isParent()){
+							tree++;
+						}
+					}else{
+						acorn++;
+					}
+				}
+			}
+			System.out.println("There are " + tree + " tree(s) and " + acorn + " acorn(s).");
+		}
+		
+	}
+
+}
