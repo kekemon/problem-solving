92205c44fc426362d420b631eaef61a47c92e591
emon <emon@DESKTOP-EULR5L1>
2018-05-03 00:52:26 +0600

    AVLTree updated

--- src/AVLTreeDemo.java
+++ src/AVLTreeDemo.java
@@ -45,13 +45,102 @@ public class AVLTreeDemo {
 			this.height = height;
 		}
 
+		private int getBalance(){
+			if(left != null && right != null) return left.getHeight() - right.getHeight();
+			if(left != null) return left.getHeight();
+			if(right != null) return -right.getHeight();
+			return 0;
+		}
+		
+		private void updateHeight() {
+			int leftHeight = 0;
+			int rightHeight = 0;
+			
+			if(left != null) leftHeight = left.getHeight();
+			if(right != null) rightHeight = right.getHeight();
+			
+			setHeight(rightHeight>leftHeight ? rightHeight : leftHeight);
+		}
+		
 		public Node insert(int value) {
-			// TODO Auto-generated method stub
-			return null;
+			//insert like BST
+			if(this.value < value){
+				if(right != null){
+					right = right.insert(value);
+				}else{
+					right = new Node(value);
+				}
+			}else if(this.value > value){
+				if(left != null){
+					left = left.insert(value);
+				}else{
+					left = new Node(value);
+				}
+			}else{
+				//No duplicate allowed
+				return this;
+			}
+			
+			//updating height while backtracking.
+			updateHeight();
+			
+			return balanceInsert(value);
+		}
+
+		private Node balanceInsert(int value) {
+			//balancing tree while backtracking.
+			int balance = getBalance();
+			
+			//LL case
+			if(balance > 1 && left.getValue() > value){
+				return rotateRight();
+			}
+			
+			//LR case
+			if(balance > 1 && left.getValue() < value){
+				left = left.rotateLeft();
+				return rotateRight();
+			}
+			
+			//RR case
+			if(balance < -1 && right.getValue() < value){
+				return rotateLeft();
+			}
+			
+			//RL case
+			if(balance < -1 && right.getValue() > value){
+				right = right.rotateRight();
+				return rotateLeft();
+			}
+			
+			//if no balance needed
+			return this;
+		}
+
+		/*
+	     y                               x
+        / \     Right Rotation          /  \
+       x   T3   – – – – – – – >        T1   y 
+      / \       < - - - - - - -            / \
+     T1  T2     Left Rotation            T2  T3
+		*/
+		
+		private Node rotateLeft() {
+			return this;
+		}
+		
+		private Node rotateRight() {
+			return this;
+		}
+		
+		public void print(){
+			if(left != null) left.print();
+			System.out.print(value + " ");
+			if(right != null) right.print();
 		}
 	}
 	
-	class AVLTree{
+	static class AVLTree{
 		private Node root;
 		
 		public AVLTree() {
@@ -65,6 +154,27 @@ public class AVLTreeDemo {
 				root = root.insert(value);
 			}
 		}
+		
+		public void print(){
+			if(root != null){
+				System.out.println();
+				root.print();
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		AVLTree avTree = new AVLTree();
+		
+		avTree.insert(10);
+		avTree.insert(20);
+		avTree.insert(30);
+		avTree.insert(40);
+		avTree.insert(50);
+        avTree.insert(60);
+        avTree.insert(70);
+		
+		avTree.print();
 	}
 
 }
